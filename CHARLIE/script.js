d0ea77277220567c0e7683ef1d4bda117a4cb436
ZSPC <121202236+ZaidSantiago@users.noreply.github.com>
2023-12-11 10:35:37 -0800

    Fixed Icons not being sent

--- CHARLIE/script.js
+++ CHARLIE/script.js
@@ -6,7 +6,7 @@ const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
 
 const botImageURL = '/assets/bot-61bdb6bf.svg';
-const userImageURL = "img/user.svg";
+const userImageURL = "/assets/user-bcdeb18e.svg";
 let loadInterval
 
 document.getElementById('backgroundButton').addEventListener('click', function () {
