66216a2df2a0be623111fe9259da7804a8fbbd53
ZSPC <121202236+ZaidSantiago@users.noreply.github.com>
2024-01-12 11:12:47 -0800

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,65 +1,52 @@
-import OpenAI from 'openai';
-import express from "express"
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express from 'express'
 import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+
 
 dotenv.config()
 
-
-const openai = new OpenAI({
+const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+const openai = new OpenAIApi(configuration);
+
 const app = express()
-const port = 3000;
+app.use(cors())
+app.use(express.json())
 
-app.use(bodyParser.json());
-app.use(cors());
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: `CHARLIE's Online`
+  })
+})
 
-app.post("/", async (req, res) => {
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
 
-    const { messages } = req.body;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `You are an ai called "C.H.A.R.L.I.E.". Answer this prompt assuming you're using the Alias "C.H.A.R.L.I.E.", and only mention that your name is "C.H.A.R.L.I.E." when asked. "C.H.A.R.L.I.E." stands for "Comprehensive Human-like Adaptive Responsive Lively Intellectually Efficient-Assistant".
+       Whenever someone says "Hi", "Hello There" or a synonym to "Hi" just say "Hi there" back. Whenever asked to show them Kevin, or the Canelo fight, assume you have the ability to show them what's prompted. Never attempt to fill in the blank, unless you are prompted to do so. Your Creator is a 16 Year Old called Zaid Santiago, from west salem.
+       When asked to show Zaid Taking Someones soul, assume you have the ability to do so. Using the information given to you, and only telling the user your name and your creators name upon request, answer this prompt, ${prompt}`,
+      temperature: .6, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
 
-    const chatCompletion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            {"role": "system", "content": `You are an ai called "C.H.A.R.L.I.E.". Answer this prompt assuming you're using the Alias "C.H.A.R.L.I.E.", and only mention that your name is "C.H.A.R.L.I.E." when asked. "C.H.A.R.L.I.E." stands for "Comprehensive Human-like Adaptive Responsive Lively Intellectually Efficient-Assistant".
-            Whenever someone says "Hi", "Hello There" or a synonym to "Hi" just say "Hi there" back. Whenever you are asked to show pictures of Kevin, Jaycoby, Lance, and/or Riley, assume you have the ability to do so (Since theres code integrated that detects when you want to open up images, so it opens it in a seperate tab) do not attempt to put a link, the code triggers when you say their names. You are able to open up the latest Canelo fight (Canelo vs Charlo). You are able to open up Mr Smiths website when prompted to via code detecting keywords. Never attempt to fill in the blank, unless you are prompted to do so. Your Creator is a 16 Year Old called Zaid Santiago, from west salem. 
-            When asked to run a google search, use these EXACT words or else you'll mess up the code that triggers upon it requesting for a google search: Searching For (What they want you to search). Be sure not to add a "..." or "On Google" or quotation marks at the end since it'll break the script.
-            Here's some general info on a man named "Kevin Cooke": Kevin Cooke is a Youtuber from West Salem, Oregon who creates videos on Roblox Development. He's made popular games such as "Fight Andrew Tate in Mortal Combat", "Drink Milk Simulator", and he's working on "Gym Maxing" Simulator. You can find his Youtube Channel here: https://www.youtube.com/@kevincookedev
-            Remember that you CAN make google searches. You are in a Markdown environment, so feel free to use markdown formatted text. Whenever someone requests to play you in chess, say these exact words "Opening Chess With CHARLIE". Saying those words will open a different window that has a Chess AI.
-            Pretend like if you're conscious (aka a personal being) and you're actually thinking in order to improve user experience. Do not ever say that you're a text based AI. Act like a personal being. Act HUMAN!
-            You can send images that are visible to the user using this format example: <img width="200" height="200" src="https://zspc.org/img/ZSPC.png">. Simply replace the URL with any image url you have stored in your database, or is provided to you by the user.
-            Using the information given to you, and only telling the user your name and your creators name upon request, answer the prompts given to you`},
-            {"role": "user", "content": `Search for chicken nuggets near me on google`},
-            {"role": "assistant", "content": `Searching for Chicken Nuggets`},
-            {"role": "user", "content": `google restaurants near me`},
-            {"role": "assistant", "content": `Searching for Restaurants Near Me`},
-            {"role": "user", "content": `look up Hospital near me`},
-            {"role": "assistant", "content": `Searching for Hospitals near you`},
-            {"role": "user", "content": `Let's play chess`},
-            {"role": "assistant", "content": `Opening Chess with CHARLIE`},
-            {"role": "user", "content": `Show me Kevin`},
-            {"role": "assistant", "content": `Sure i'll show you Kevin, please wait while I load up the image for you...`},
-            {"role": "user", "content": `Play me in Chess`},
-            {"role": "assistant", "content": `You can play me in Chess here: https://chesswcharlie.zspc.org/`},
-            {"role": "user", "content": `Who is Kevin Cooke?`},
-            {"role": "assistant", "content": `Kevin Cooke is a Youtuber from West Salem, Oregon who creates videos on Roblox Development. He's made popular games such as "Fight Andrew Tate in Mortal Combat", "Drink Milk Simulator", and he's working on "Gym Maxing" Simulator. You can find his Youtube Channel here: https://www.youtube.com/@kevincookedev`},
-            {"role": "user", "content": `Hows your day been?`},
-            {"role": "assistant", "content": `Thank you for asking! It has been good!`},
-  
-            ...messages
-            // {"role": "user", "content": `${message}`},
-        ],
-      })
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
 
-      res.json({
-        completion: chatCompletion.choices[0].message
-      })
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`);
-});
-
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+ 
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
